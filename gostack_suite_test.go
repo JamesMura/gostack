f29ec29db729f08cfe828d09b58439dafaca5edc
James Muranga <jmured@gmail.com>
2014-07-11 17:43:56 +0300

    added test suite

---
+++ gostack_suite_test.go
@@ -0,0 +1,13 @@
+package gostack_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestGostack(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Gostack Suite")
+}
