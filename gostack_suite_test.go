ae9f8c571db0288a651cce8208bbbf86d072f37f
James Muranga <jmured@gmail.com>
2014-07-13 22:53:35 +0300

    moving files around

--- gostack_suite_test.go
+++ gostack_suite_test.go
@@ -1,13 +1,13 @@
 package gostack_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
 
-	"testing"
+    "testing"
 )
 
 func TestGostack(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Gostack Suite")
+    RegisterFailHandler(Fail)
+    RunSpecs(t, "Gostack Suite")
 }
